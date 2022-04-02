a08f7ead287b58a179bfa1f7e4030462c0a3434d
rnd4web <rnd4web@msn.com>
2022-04-02 11:28:39 -0300

    TESTE DE COMMIT RSTUDIO

--- loc_sgq.R
+++ loc_sgq.R
@@ -28,7 +28,7 @@ pacman::p_load(usethis, magrittr, pacman, rio, tidyverse, lubridate)
 # Load base package (usethis)--------------------------------------------------
 
 library (usethis)
-# usethis::use_git_remote("origin", url = NULL, overwrite = FALSE)
+usethis::use_git_remote("origin", url = NULL, overwrite = FALSE)
 ### PAPAGAIO
 
 # Criar o CAMINHO ABSOLUTO
